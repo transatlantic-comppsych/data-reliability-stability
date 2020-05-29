d07026191465d43c272bcaf907dd5f3d8788d895
lilyeisner <65026119+lilyeisner@users.noreply.github.com>
2020-05-29 16:34:46 -0400

    Added an assumption test based on Heise func (15)

--- reliability_stability_pkg/reliability_stability.py
+++ reliability_stability_pkg/reliability_stability.py
@@ -41,6 +41,35 @@ def calc_stability(data, column_1, column_2, column_3):
 	
 	return(stability_12, stability_23, stability_13)
 
+#function to check the assumptions on the reliabililty and stability tests
+def assumption_test(data, column_1, column_2, column_3, column_4):
+    
+    # calculate correlation for outside columns and inside columns
+    r14 = calc_correlation(data,column_1,column_4)
+    r23 = calc_correlation(data,column_2,column_3)
+    
+    # calculate correlation for leaved columns
+    r13 = calc_correlation(data,column_1,column_3)
+    r24 = calc_correlation(data,column_2,column_4)
+
+    # calculate products of correlations
+    product_1 = r14*r23
+    product_2 = r13*r24
+    
+    # adjust correlation products and transform with fisher's z-transformation
+    if product_1 > 0:
+        adjusted_product_1 = np.arctanh(np.sqrt(np.absolute(product_1)))
+    else:
+        adjusted_product_1 = -(np.arctanh(np.sqrt(np.absolute(product_1))))
+    
+    if product_2 > 0:
+        adjusted_product_2 = np.arctanh(np.sqrt(np.absolute(product_2)))
+    else:
+        adjusted_product_2 = -(np.arctanh(np.sqrt(np.absolute(product_2))))
+    
+    # return the difference in the adjusted product correlations
+    return adjusted_product_1 - adjusted_product_2
+
 def main():
 	
 	#import data set
@@ -50,6 +79,7 @@ def main():
 	column_one = sys.argv[2]
 	column_two = sys.argv[3]
 	column_three = sys.argv[4]
+	column_four = sys.argv[5]
 
 	print(calc_reliability(data, column_one, column_two, column_three))
 	print(calc_stability(data, column_one, column_two, column_three))
