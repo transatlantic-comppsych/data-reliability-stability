213f20ca847adb894e43ad4ef536f4442be57115
lilyeisner <65026119+lilyeisner@users.noreply.github.com>
2020-05-29 13:42:22 -0400

    Update import file path

--- tests/test_stability.py
+++ tests/test_stability.py
@@ -1,21 +1,18 @@
 #import necessary python libraries
 from pathlib import Path
 import pandas as pd
-import sys
-sys.path.insert(0, "data-reliability-stability/reliability_stability_pkg")
-from reliability_stability_pkg import reliability_stability as rs
-	
+import reliability_stability as rs
     
 test_dir = Path(__file__).parent
 #import data set
 data = pd.read_csv(test_dir / 'BodyFat.csv')
 
 #define columns based on input column headers
-column_one = data.loc[:, 'BICEPS']
-column_two = data.loc[:, 'FOREARM']
-column_three = data.loc[:, 'WRIST']
+column_one = 'BICEPS'
+column_two = 'FOREARM'
+column_three = 'WRIST'
 
-(stability_12, stability_23, stability_13) = rs.calc_stability(column_one, column_two, column_three)
+(stability_12, stability_23, stability_13) = rs.calc_stability(data, column_one, column_two, column_three)
 stability_12 = float('%.3f'%(stability_12))
 stability_23 = float('%.3f'%(stability_23))
 stability_13 = float('%.3f'%(stability_13))
