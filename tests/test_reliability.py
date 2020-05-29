63e47be9421d4e829188e2ff835aea9436b1bde3
lilyeisner <65026119+lilyeisner@users.noreply.github.com>
2020-05-29 13:41:07 -0400

    Update import file path

--- tests/test_reliability.py
+++ tests/test_reliability.py
@@ -1,20 +1,18 @@
 #import necessary python libraries
 from pathlib import Path
 import pandas as pd
-import sys
-sys.path.insert(0, "/lilyeisner/Desktop/r_s_master/reliability_stability_pkg")
-from reliability_stability_pkg import reliability_stability as rs
+import reliability_stability as rs
 	
 test_dir = Path(__file__).parent
 #import data set
 data = pd.read_csv(test_dir / 'BodyFat.csv')
 
 #define columns based on input column headers
-column_one = data.loc[:, 'BICEPS']
-column_two = data.loc[:, 'FOREARM']
-column_three = data.loc[:, 'WRIST']
+column_one = 'BICEPS'
+column_two = 'FOREARM'
+column_three = 'WRIST'
 
-reliability_test_value = float('%.3f'%(rs.calc_reliability(column_one, column_two, column_three)))
+reliability_test_value = float('%.3f'%(rs.calc_reliability(data, column_one, column_two, column_three)))
 
 def test_reliability_value():
     assert reliability_test_value == 0.628
